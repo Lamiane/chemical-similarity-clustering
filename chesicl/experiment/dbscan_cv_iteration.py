a15ff5c568dd768e87d92f466c2917f5945e7d67
Agnieszka Pocha <lamiane.lammy@gmail.com>
2017-01-31 22:07:43 +0100

    dbscan experiment added

--- chesicl/experiment/dbscan_cv_iteration.py
+++ chesicl/experiment/dbscan_cv_iteration.py
@@ -2,7 +2,7 @@
 # now the line above is super important!
 
 # define single iteration of cross-validation
-from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
 import numpy as np
 import os
 import logging
@@ -11,18 +11,16 @@ logging.basicConfig()
 from drgmum.toolkit import burrito
 
 from chesicl.data_loading.exp_data_loader import load_data
-from experiment import scoring_function
+from experiment import scoring_function, random_state
 
 # default parameters are must-be!
-def cv_iteration(n_clusters=1, n_init=1, init='random', algorithm='elkan', max_iter=600):
+def cv_iteration(n_jobs=4, eps=1, min_samples=30, metric='euclidean', algorithm='brute', leaf_size=30, p=2):
     X, y_train = load_data()
     scores = []
-    random_states = (666, 69, 7, 13, 1337)
-    for i in random_states:
-        model = KMeans(n_clusters=n_clusters, init=init, n_init=n_init, algorithm=algorithm, random_state=i)
-        model.fit(X)
-        predictions = model.predict(X)
-        scores.append(scoring_function(y_train, predictions))
+    model = KMeans(n_jobs=n_jobs, eps=eps, min_samples=min_samples, metric=metric, algorithm=algorithm, leaf_size=leaf_size, p=p)
+    model.fit(X)
+    predictions = model.predict(X)
+    scores.append(scoring_function(y_train, predictions))
     return {'result': scores}
 
 if __name__ == "__main__":
--- chesicl/experiment/experiment.py
+++ chesicl/experiment/experiment.py
@@ -1,7 +1,8 @@
 import os
+import numpy as np
 from drgmum.toolkit.job_pool import JobPool, dict_hash
 from drgmum.toolkit import burrito
-from kmeans_cv_iteration import hiperparameters as kmeans_hiperparameters
+from scorer import my_binary_score
 
 def runner(script_name, script_params, **kwargs):
     # Assumes script is wrapped in src.script_wrapper
@@ -16,8 +17,49 @@ def runner(script_name, script_params, **kwargs):
     if ret != 0:
         raise RuntimeError("Failed running cmd")
 
+# config
+output_data_dir = "../.."
+scripts_dir = "/home/pocha/chemical-similarity-clustering/chesicl/experiment"
+scoring_function = my_binary_score  # used in each file definig cv_iteration
+max_clusters = 200
+n_clusters = list(range(1, max_clusters))
+metrics_space =  ['cityblock', 'cosine', 'euclidean', 'l1', 'l2', 'manhattan'] + ['braycurtis', 'canberra', 'chebyshev', 'correlation', 'dice', 'hamming', 'jaccard', 'kulsinski', 'mahalanobis', 'matching', 'minkowski', 'rogerstanimoto', 'russellrao', 'seuclidean', 'sokalmichener', 'sokalsneath', 'sqeuclidean', 'yule']
+     
+    
 # KMEANS
-job_pool = JobPool(n_jobs=10, output_dir='KMEANS')
-jobs = [{"script_name": "/home/pocha/chemical-similarity-clustering/kmeans_cv_iteration.py", "script_params": h} for h in kmeans_hiperparameters]
-keys = [str(j["script_params"]) for j in jobs]
-job_pool.map(runner, jobs, keys)
+# define hiperparameters
+init =['k-means++', 'random']
+algorithm = ['full', 'elkan']
+kmeans_hiperparameters = [{'n_clusters':nc, 'init':init_, 'algorithm':alg, 'n_init':1, 'max_iter':600} 
+                   for nc in n_clusters for init_ in init for alg in algorithm]
+# run jobs
+kmeans_job_pool = JobPool(n_jobs=10, output_dir=os.path.join(output_data_dir, 'KMEANS'))
+kmeans_jobs = [{"script_name": os.path.join(scripts_dir, "kmeans_cv_iteration.py"), "script_params": h} for h in kmeans_hiperparameters]
+kmeans_keys = [str(j["script_params"]) for j in kmeans_jobs]
+kmeans_job_pool.map(runner, kmeans_jobs, kmeans_keys)
+
+del kmeans_hiperparameters, kmeans_jobs, kmeans_keys, init, algorithm
+        
+
+# DBSCAN
+# define hiperparameters
+low = -10
+high = 5
+eps = np.logspace(low, high, np.abs(low)+np.abs(high)+1)
+min_samples = xrange(2, 11)
+metric = list(metrics_space)
+algorithm = ['ball_tree', 'kd_tree', 'brute']
+leaf_size = [3, 10, 20, 30]
+p = xrange(1, 11)
+n_jobs=4
+
+dbscan_hiperparameters=[{'n_jobs':n_jobs, 'eps':e, 'min_samples':m_s, 'metric':mtr, 'algorithm':alg, 'leaf_size':l_s, 'p':_p} for e in eps for m_s in min_samples for mtr in metric for alg in algorithm for l_s in leaf_size for _p in p]
+
+# run jobs
+dbscan_job_pool = JobPool(n_jobs=10, output_dir=os.path.join(output_data_dir, 'KMEANS'))
+dbscan_jobs = [{"script_name": os.path.join(scripts_dir, "dbscan_cv_iteration.py"), "script_params": h} for h in dbscan_hiperparameters]
+dbscan_keys = [str(j["script_params"]) for j in dbscan_jobs]
+dbscan_job_pool.map(runner, dbscan_jobs, dbscan_keys)
+
+del low, high, eps, min_samples, metric, algorithm, leaf_size, p, n_jobs, dbscan_hiperparameters, dbscan_keys
+
